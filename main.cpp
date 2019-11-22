fde06de60f3eeab80d7b5960b2229ea0a527e30c
pberrigan <58068410+pberrigan@users.noreply.github.com>
2019-11-22 02:57:22 -0600

    Add files via upload
    Program 2: This program will calculate the sales prediction for a Company Division.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 // Patrick Berrigan
 // CSCI 1933 M02
-// Program 1: This program will calculate the sum of two numbers and display the result.
-
+// Program 2: This program will calculate the sales prediction for a Company Division.
 
 #include <iostream>
 using namespace std;
@@ -9,17 +8,20 @@ using namespace std;
 int main()
 {
     // Variable Declaration
-    int num1, num2, total;
+    double totalSales, percentDivision, result;
 
     // Input
-    num1 = 50;
-    num2 = 100;
+    percentDivision = .58;
+    totalSales = 8.6e6;
 
     // Processing
-    total = num1 + num2;
+    result = totalSales * percentDivision;
 
     // Output
-    cout << "The total is: " << total << endl;
+    cout << "East Coast Division Outlook:" << endl;
+    cout << "The East Coast Division is predicted to generate $" << result << " in revenue this year." << endl;
+    cout << "This prediction is based on the East Coast Division accounting for " << (percentDivision*100) << "% average yearly sales" << endl;
+    cout << "and the company's projected $" << (totalSales) << " in total sales for the end of this fiscal year." << endl;
 
     return 0;
 }
