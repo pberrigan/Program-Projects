a50c4af8f141134b98de9017348815e1c0f4a69e
pberrigan <58068410+pberrigan@users.noreply.github.com>
2019-11-22 02:43:33 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// Patrick Berrigan
+// CSCI 1933 M02
+// Program 1: This program will calculate the sum of two numbers and display the result.
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Variable Declaration
+    int num1, num2, total;
+
+    // Input
+    num1 = 50;
+    num2 = 100;
+
+    // Processing
+    total = num1 + num2;
+
+    // Output
+    cout << "The total is: " << total << endl;
+
+    return 0;
+}
